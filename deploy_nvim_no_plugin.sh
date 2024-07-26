c03219dfe39fb704306d8e6c72236f7910a1772a
Siyuan Wang <12012324@mail.sustech.edu.cn>
2024-07-26 14:12:45 +0800

    update deploy scripts

--- deploy_nvim_no_plugin.sh
+++ deploy_nvim_no_plugin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget -P ~/.config/nvim/ https://gitee.com/seahipage/highvim/raw/main/highvim/no-plugin-version/nvim/nvim-linux64.tar.gz
-tar -xzvf ~/.config/nvim/nvim-linux64.tar.gz -C ~/.config/nvim/
+wget -P ~/.config/nvim/ https://gitee.com/seahipage/highvim/raw/main/highvim/no-plugin-version/nvim/nvim-linux64.tar.xz
+tar -xJvf ~/.config/nvim/nvim-linux64.tar.xz -C ~/.config/nvim/
 
 # wget -P ~/.config/nvim  https://raw.githubusercontent.com/SeaHI-Robot/highvim/main/no-plugin-version/nvim/init.vim
 # wget -P ~/.config/nvim  https://raw.githubusercontent.com/SeaHI-Robot/highvim/main/no-plugin-version/.vimrc
-wget -P ~/.config/nvim  https://gitee.com/seahipage/highvim/raw/main/no-plugin-version/nvim/init.vim
-wget -P ~/.config/nvim  https://gitee.com/seahipage/highvim/raw/main/no-plugin-version/.vimrc
+wget -P ~/.config/nvim  https://gitee.com/seahipage/highvim/raw/main/highvim/no-plugin-version/nvim/init.vim
+wget -P ~/.config/nvim  https://gitee.com/seahipage/highvim/raw/main/highvim/no-plugin-version/.vimrc
 
 echo "alias nv='~/.config/nvim/nvim-linux64/bin/nvim'" >> ~/.bash_aliases
 
