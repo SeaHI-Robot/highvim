35bdd3188bdb25495bc01a59bb521f0d6adee4a4
Siyuan Wang <12012324@mail.sustech.edu.cn>
2024-10-13 17:02:03 +0800

    update ros2 plugin

--- lua/plugins/ros2.lua
+++ lua/plugins/ros2.lua
@@ -5,6 +5,14 @@ return {
         "nvim-telescope/telescope.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
+    keys = {
+        { "<leader>Rte", "<cmd>Telescope ros2 topics_echo<cr>", desc = "ROS2: [R]os2 [t]opic [e]cho]",  mode = { "n" } }, -- This function has a bug, topic echos whill only display once
+        { "<leader>Rti", "<cmd>Telescope ros2 topics_info<cr>", desc = "ROS2: [R]os2 [t]opic [i]nfo]",  mode = { "n" } },
+        { "<leader>Rn",  "<cmd>Telescope ros2 nodes<cr>",       desc = "ROS2: [R]os2 [n]ode list",      mode = { "n" } },
+        { "<leader>Ra",  "<cmd>Telescope ros2 actions<cr>",     desc = "ROS2: [R]os2 [a]ction list",    mode = { "n" } },
+        { "<leader>Rs",  "<cmd>Telescope ros2 services<cr>",    desc = "ROS2: [R]os2 [s]ervice list",   mode = { "n" } },
+        { "<leader>Ri",  "<cmd>Telescope ros2 interfaces<cr>",  desc = "ROS2: [R]os2 [i]nterface list", mode = { "n" } },
+    },
     opts = {
         -- Add any custom options here
         autocmds = true,
